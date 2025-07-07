e35f68d085b8823ed9eaf08ee7e2b18dc6581e63
Kyle Beyer <beyerk@frib.msu.edu>
2025-07-07 13:53:13 -0400

    add cont scripts

--- calibration/continue_cal.py
+++ calibration/continue_cal.py
@@ -11,66 +11,51 @@ from mpi4py import MPI
 
 
 def main():
-    """Main function to run the walkers in parallel, starting with state from a single
-    walker provided as input"""
-
-    parser = argparse.ArgumentParser(description="Run a parallel walker simulation.")
-    parser.add_argument(
-        "--input",
-        type=str,
-        help="Input file path for walker; expected to be an lzma-compressed pickle file",
-    )
-    parser.add_argument(
-        "--output",
-        type=str,
-        default="./",
-        help="Output directory for results",
-    )
-    parser.add_argument(
-        "--steps", type=int, default=10000, help="Number of walking steps."
-    )
-    parser.add_argument(
-        "--batch_size", type=int, default=None, help="Batch size for walker."
-    )
-    parser.add_argument(
-        "--burnin", type=int, default=1000, help="Number of burn-in steps."
-    )
-    parser.add_argument("--verbose", action="store_true", help="Enable verbose mode.")
-
-    args = parser.parse_args()
-
-    # Verify that the input file exists
-    input_path = Path(args.input)
-    if not input_path.is_file():
-        print(f"Error: The input file '{args.input}' does not exist.")
-        sys.exit(1)
-
-    # Ensure output directory exists
-    output_path = Path(args.output)
-    output_path.mkdir(parents=True, exist_ok=True)
+    """Read walkers in parallel, advance thier state, and overwrite them """
 
     # Initialize MPI
     comm = MPI.COMM_WORLD
     rank = comm.Get_rank()
 
+    parser = argparse.ArgumentParser(description="Advance state of provided walkers")
+    parser.add_argument(
+        "--input",
+        type=str,
+        help="Input directory expected to containt one file called walker_{rank}.pkl.xz"
+            " for each rank, each one being an LZMA-compressed walker object.",
+    )
+    parser.add_argument(
+        "--steps", type=int,  help="Number of walking steps."
+    )
+    parser.add_argument(
+        "--batch_size", type=int, default=None, help="Batch size for walker."
+    )
+    parser.add_argument(
+        "--burnin", type=int,  help="Number of burn-in steps."
+    )
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose mode.")
+
+    args = parser.parse_args()
+
+    input_path = Path(args.input)
+    if not input_path.is_dir():
+        print(f"Error: The input directory '{args.input}' does not exist.")
+        sys.exit(1)
+    
     # Read input file
+    input_file = input_path / f"walker_{rank}.pkl.xz"
     try:
-        with lzma.open(input_path, "rb") as f:
+        with lzma.open(input_file, "rb") as f:
             walker = pickle.load(f)
     except Exception as e:
         print(
-            f"Error: Failed to read input file '{args.input}' on rank {rank}. Exception: {e}"
+            f"Error: Failed to read input file '{ininput_file}' on rank {rank}. Exception: {e}"
         )
         sys.exit(1)
-
-    # Set RNG seed for each walker
-    try:
-        walker.rng = np.random.default_rng(rank)
-    except Exception as e:
-        print(
-            f"Error: Failed to initialize random number generator on rank {rank}. Exception: {e}"
-        )
-        sys.exit(1)
+    
+    # Ensure output directory exists
+    output_path = Path(args.output)
+    output_path.mkdir(parents=True, exist_ok=True)
 
     # Run the walker for the specified number of steps
     print(f"walking on rank {rank}...")
